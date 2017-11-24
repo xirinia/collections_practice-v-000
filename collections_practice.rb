7ab470adf87c6b3c611b829e34421586d0f12797
Anaxirinia Jimenez <anax131@gmail.com>
2017-11-24 18:06:00 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,33 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array.sort do |a, b|
+    if a == array[1]
+      a==array[2]
+    end
+  end
+end
+
+def reverse_array(array)
+  newArray = []
+  i= array.length
+  while i > 0
+    newArray << array[i-1]
+    i -=1
+  end
+  newArray
+end
